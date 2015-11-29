b50d2ada97ed700139b05101cce165a3b1f7ebef
Kerem Kazan <kerem.kazan@gmail.com>
2015-11-29 13:53:04 -0500

    logger fixed

--- logger.cpp
+++ logger.cpp
@@ -1,37 +1,33 @@
 #include "logger.h"
-#include "mbed.h"
-#include <string>
 LocalFileSystem local("local");
 
 int Logger::logfileno = 0;
+FILE* Logger::logfile = NULL;
 
 void Logger::create_log_file(char *c) {
     char filename[64];    
     int n = 0;
     
-    // set "filename" equal to the next file to write
     while(1) {
-        sprintf(filename, "/local/log%03d.txt", n);    // construct the filename fileNNN.txt
-        FILE *fp = fopen(filename, "r");                // try and open it
-        if(fp == NULL) {                                // if not found, we're done!
+        sprintf(filename, "/local/log%03d.txt", n);
+        FILE *fp = fopen(filename, "r");
+        if(fp == NULL) {
             break;
         }
-        fclose(fp);                                     // close the file
-        n++;                                            // and try the next one
+        fclose(fp);
+        n++;
     }
     
-    
     Logger::logfileno = n;
-    FILE *fp = fopen(filename, "w");
-    fprintf(fp, "Logfile #%d \n",n);
-    fprintf(fp, "%s \n",c);
-    fclose(fp);
+    Logger::logfile = fopen(filename, "w");
+    fprintf(Logger::logfile, "%s \n",c);
+    fprintf(Logger::logfile, "Log #%d \n",n);
 }
 
 void Logger::log(char *c) {
-    char filename[64]; 
-    sprintf(filename, "/local/log%03d.txt", Logger::logfileno); 
-    FILE *fp = fopen(filename, "a");
-    fprintf(fp,"%s\n", c);
-    fclose(fp);
+    fprintf(Logger::logfile,"%s\n", c);
+}
+
+void Logger::close_log_file() {
+    fclose(Logger::logfile);    
 }
--- logger.h
+++ logger.h
@@ -1,15 +1,19 @@
 #ifndef LOGGER_H
 #define LOGGER_H
 
+#include "mbed.h"
+
 class Logger {
     public:
         
     static void log(char *c);
     static void create_log_file(char *c);
+    static void close_log_file();
     
     private:
     
     static int logfileno;
+    static FILE *logfile;
     
 };
 
